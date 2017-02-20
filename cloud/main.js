716d6ea1fde9be135731bb001d74e1d32e9c4b36
yhu98 <yhu98@ucsc.edu>
2017-02-19 17:39:17 -0800

    Someone liked your post

--- cloud/main.js
+++ cloud/main.js
@@ -20,7 +20,7 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
   Parse.Push.send({
     where: pushQuery, // Set our Installation query
     data: {
-      alert: "Message: " + messageText
+      alert: "Message:Someone liked your post "
     }
   }, { success: function() {
       console.log("#### PUSH OK");
